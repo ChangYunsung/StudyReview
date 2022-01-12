02c2b3660e3567773b69f6c9f636f462c28e7f60
ChangYunsung <74763204+ChangYunsung@users.noreply.github.com>
2022-01-12 18:11:35 +0900

    Add files via upload
    12_06

---
+++ com/gd/test/bean/re_12_06_TestBean.java
@@ -0,0 +1,33 @@
+package com.gd.test.bean;
+
+public class re_12_06_TestBean {
+	
+	private int num;
+	private String title;
+	private String writer;
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getWriter() {
+		return writer;
+	}
+	
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+}
