b543d4aad691c01cf87ee85375b63999676d517d
ChangYunsung <74763204+ChangYunsung@users.noreply.github.com>
2022-01-13 11:44:05 +0900

    Add files via upload
    12_08

---
+++ re_12_08.java
@@ -0,0 +1,9 @@
+package review;
+/*
+ * 12¿ù 08 º¹½À
+ * fianl
+ */
+
+public class re_12_08 {
+	public final String msg2 = null;
+}
---
+++ re_12_08_2.java
@@ -0,0 +1,13 @@
+package review;
+/*
+ * 12¿ù 08 º¹½À
+ * fianl
+ */
+
+
+public class re_12_08_2 {
+	public static void main(String[] args) {
+		re_12_08 ts = new re_12_08();
+		System.out.println(ts.msg2);
+	}
+}
